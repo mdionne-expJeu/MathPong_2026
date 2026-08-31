ddf4c49f872cb7d85a85cfdb789c9e0aee765cb3
mathevos <79484867+mathevos@users.noreply.github.com>
2026-08-31 14:19:12 -0400

    Ajout de la gestion de la fin de partie (RPC)
    Avec possibilité de recommencer une nouvelle partie.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,20 +20,20 @@ public class GameManager : NetworkBehaviour //pour un network object
             Destroy(gameObject);
         }
     }
-    
-    // L'hôte de la partie attend que 2 joueurs soient connectés pour lancer la partie
-   // Seulement l'hôte exécute ce code
-   // Aucune vérification si partie déjà en cours
-   void Update()
-   {
-       if (!IsHost) return;
-       if (partieEnCours) return;
 
-       if (NetworkManager.Singleton.ConnectedClientsList.Count >= 2)
-       {
-           NouvellePartie();
-       }
-   }
+    // L'hôte de la partie attend que 2 joueurs soient connectés pour lancer la partie
+    // Seulement l'hôte exécute ce code
+    // Aucune vérification si partie déjà en cours
+    void Update()
+    {
+        if (!IsHost) return;
+        if (partieEnCours) return;
+
+        if (NetworkManager.Singleton.ConnectedClientsList.Count >= 2)
+        {
+            NouvellePartie();
+        }
+    }
 
 
     // Fonction appelée pour le bouton qui permet de se connecter comme hôte
@@ -49,17 +49,27 @@ public class GameManager : NetworkBehaviour //pour un network object
     }
 
     // Activation d'une nouvelle partie lorsque 2 joueurs. On appelle la fonction de la balle qui
-   // la place au milieu et qui lui donne une vélocité.
-   public void NouvellePartie()
-   {
-       partieEnCours = true;
-       BalleRigid.instance.LanceBalleMilieu();
-   }
+    // la place au milieu et qui lui donne une vélocité.
+    public void NouvellePartie()
+    {
+        partieEnCours = true;
+        BalleRigid.instance.LanceBalleMilieu();
+    }
 
-  // Fonction appelée par le ScoreManager pour terminer la partie (nous l'utilserons plus tard)
-   public void FinPartie()
-   {
-       partieTerminee = true;
-   }
+    // Fonction appelée par le ScoreManager pour terminer la partie (nous l'utilserons plus tard)
+    public void FinPartie()
+    {
+        partieTerminee = true;
+    }
+
+    // Fonction appelée par le bouton Recommencer pour recommencer une partie
+    public void Recommencer()
+    {
+        NetworkManager.Singleton.Shutdown(); // On arrête le NetworkManager pour réinitialiser la partie
+        Destroy(NetworkManager.gameObject);
+        partieEnCours = false; // On remet la partie en cours à false
+        SceneManager.LoadScene(0);// On recharge la scène de jeu
+    }
+
 
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,85 +6,143 @@ using Unity.Netcode;
 // Script pour gérer le score du jeu et gérer la fin de partie
 public class ScoreManager : NetworkBehaviour // ne pas oublier component networkObject
 {
-   public static ScoreManager instance; // singleton
-   [SerializeField] private TMP_Text scoreTxt; // Référence à la zone qui affiche le texte. À définir dans l'inspecteur
-   [SerializeField] private int pointageCible; // Le pointage à atteindre pour gagner (servira plus tard)
+    public GameObject pannelVictoire; // Référence au panel pour la victoire
+    public GameObject pannelDefaite; // Référence au panel pour la défaite
+    public static ScoreManager instance; // singleton
+    [SerializeField] private TMP_Text scoreTxt; // Référence à la zone qui affiche le texte. À définir dans l'inspecteur
+    [SerializeField] private int pointageCible; // Le pointage à atteindre pour gagner (servira plus tard)
 
-   private NetworkVariable<int> scoreHote = new NetworkVariable<int>(); // Score de l'hôte (variable réseau)
-   private NetworkVariable<int> scoreClient = new NetworkVariable<int>(); // Score du client (variable réseau)
+    private NetworkVariable<int> scoreHote = new NetworkVariable<int>(); // Score de l'hôte (variable réseau)
+    private NetworkVariable<int> scoreClient = new NetworkVariable<int>(); // Score du client (variable réseau)
 
-   // Création du singleton
-   private void Awake()
-   {
-       if (instance == null)
-       {
-           instance = this;
-       }
-       else
-       {
-           Destroy(gameObject);
-       }
-   }
+    // Création du singleton
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-   /*Méthode appelée lors du spawn de l'objet réseau
-   - Initialise les scores à 0 si c'est le serveur
-   - S'abonne aux événements de changement de valeur des scores */
-   public override void OnNetworkSpawn()
-   {
-       base.OnNetworkSpawn();
-       
-       if (IsServer)
-       {
-           scoreHote.Value = 0;
-           scoreClient.Value = 0;
-       }
-      
-       scoreHote.OnValueChanged += OnChangementPointageHote;
-       scoreClient.OnValueChanged += OnChangementPointageClient;
-   }
+    /*Méthode appelée lors du spawn de l'objet réseau
+    - Initialise les scores à 0 si c'est le serveur
+    - S'abonne aux événements de changement de valeur des scores */
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
 
-   /* Méthode appelée lors de la désactivation de l'objet réseau
-   - Se désabonne des événements de changement de valeur des scores */
-   public override void OnNetworkDespawn()
-   {
-       base.OnNetworkDespawn();
-       
-       scoreHote.OnValueChanged -= OnChangementPointageHote;
-       scoreClient.OnValueChanged -= OnChangementPointageClient;
-   }
+        if (IsServer)
+        {
+            scoreHote.Value = 0;
+            scoreClient.Value = 0;
+        }
 
-   /* Fonction pour augmenter le score de l'hôte
-    - On incrémente le score de l'hôte
-    - On vérifie si la partie est terminée*/
-   public void AugmenteHoteScore() // Fonction public. À implémenter La balle doit appeler cette fonction lorsqu'un but est compté par l'hôte
-   {
-       scoreHote.Value++;
-   }
+        scoreHote.OnValueChanged += OnChangementPointageHote;
+        scoreClient.OnValueChanged += OnChangementPointageClient;
+    }
 
-   /* Fonction pour augmenter le score du client
-    - On incrémente le score du client
-    - On vérifie si la partie est terminée*/
-   public void AugmenteScoreClient() // Fonction public. À implémenter La balle doit appeler cette fonction lorsqu'un but est compté par le client
-   {
-       scoreClient.Value++;
-   }
+    /* Méthode appelée lors de la désactivation de l'objet réseau
+    - Se désabonne des événements de changement de valeur des scores */
+    public override void OnNetworkDespawn()
+    {
+        base.OnNetworkDespawn();
 
-   // Méthode pour gérer le changement de valeur du score de l'hôte
-   // Elle est appelée automatiquement à chaque fois que le score de l'hôte change
-   // Elle met à jour le texte affiché avec les scores actuels
-   private void OnChangementPointageHote(int ancienScoreHote, int nouveauScoreHote)
-   {
-       if (ancienScoreHote == nouveauScoreHote) return; // Évite de mettre à jour si le score n'a pas changé
+        scoreHote.OnValueChanged -= OnChangementPointageHote;
+        scoreClient.OnValueChanged -= OnChangementPointageClient;
+    }
 
-       scoreTxt.text = scoreHote.Value + " - " + scoreClient.Value;
-   }
+    /* Fonction pour augmenter le score de l'hôte
+     - On incrémente le score de l'hôte
+     - On vérifie si la partie est terminée*/
+    public void AugmenteHoteScore() // Fonction public. À implémenter La balle doit appeler cette fonction lorsqu'un but est compté par l'hôte
+    {
+        scoreHote.Value++;
+        VerifieFinPartie();
+    }
 
-   // Méthode pour gérer le changement de valeur du score du client
-   // Elle est appelée automatiquement à chaque fois que le score du client change
-   // Elle met à jour le texte affiché avec les scores actuels
-   private void OnChangementPointageClient(int ancienScoreClient, int nouveauScoreClient)
-   {
-       if (ancienScoreClient == nouveauScoreClient) return; // Évite de mettre à jour si le score n'a pas changé
-       scoreTxt.text = scoreHote.Value + " - " + scoreClient.Value;
-   }
+    /* Fonction pour augmenter le score du client
+     - On incrémente le score du client
+     - On vérifie si la partie est terminée*/
+    public void AugmenteScoreClient() // Fonction public. À implémenter La balle doit appeler cette fonction lorsqu'un but est compté par le client
+    {
+        scoreClient.Value++;
+        VerifieFinPartie();
+    }
+
+    // Méthode pour gérer le changement de valeur du score de l'hôte
+    // Elle est appelée automatiquement à chaque fois que le score de l'hôte change
+    // Elle met à jour le texte affiché avec les scores actuels
+    private void OnChangementPointageHote(int ancienScoreHote, int nouveauScoreHote)
+    {
+        if (ancienScoreHote == nouveauScoreHote) return; // Évite de mettre à jour si le score n'a pas changé
+
+        scoreTxt.text = scoreHote.Value + " - " + scoreClient.Value;
+    }
+
+    // Méthode pour gérer le changement de valeur du score du client
+    // Elle est appelée automatiquement à chaque fois que le score du client change
+    // Elle met à jour le texte affiché avec les scores actuels
+    private void OnChangementPointageClient(int ancienScoreClient, int nouveauScoreClient)
+    {
+        if (ancienScoreClient == nouveauScoreClient) return; // Évite de mettre à jour si le score n'a pas changé
+        scoreTxt.text = scoreHote.Value + " - " + scoreClient.Value;
+    }
+
+
+    /* Fonction pour vérifier si la partie est terminée
+    - Si le score de l'hôte ou du client atteint le pointage cible, on affiche le panel de victoire ou de défaite
+    - On appelle la fonction GagnantHote_ClientRpc ou GagnantClient_ClientRpc selon le cas
+    - On appelle la fonction FinPartie du GameManager pour terminer la partie */
+
+    void VerifieFinPartie()
+    {
+        Debug.Log("vérification de fin de partie");
+        if (scoreHote.Value >= pointageCible)
+        {
+            GagnantHote_ClientRpc();
+            GameManager.instance.FinPartie();
+        }
+        else if (scoreClient.Value >= pointageCible)
+        {
+            GagnantClient_ClientRpc();
+            GameManager.instance.FinPartie();
+        }
+    }
+
+    /* Fonction RPC pour afficher le panel de victoire pour l'hôte et le panel de défaite pour le client
+     - Appelée par le serveur pour tous les clients */
+    [Rpc(SendTo.Everyone)]
+    private void GagnantHote_ClientRpc()
+    {
+        Debug.Log("GagnantHote_ClientRpc");
+        if (IsServer)
+        {
+            pannelVictoire.SetActive(true);
+        }
+        else
+        {
+            pannelDefaite.SetActive(true);
+        }
+    }
+    /* Fonction RPC pour afficher le panel de victoire pour le client et le panel de défaite pour l'hôte
+     - Appelée par le serveur pour tous les clients */
+    [Rpc(SendTo.Everyone)]
+    private void GagnantClient_ClientRpc()
+    {
+        Debug.Log("GagnantClient_ClientRpc");
+        if (IsServer)
+        {
+            pannelDefaite.SetActive(true);
+        }
+        else
+        {
+            pannelVictoire.SetActive(true);
+        }
+    }
 }
+
+
