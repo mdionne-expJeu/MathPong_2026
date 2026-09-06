96ab2735252c4723a0f72ca3293fd8fae7ce22dd
mathevos <79484867+mathevos@users.noreply.github.com>
2026-09-06 11:20:06 -0400

    Camshaker et augmentation vitesse
    Mise à jour Unity 6.5.9f1

--- Assets/Scripts/BalleRigid.cs
+++ Assets/Scripts/BalleRigid.cs
@@ -8,86 +8,121 @@ using Unity.Netcode.Components;// pour accéder aux propriétés du NetworkTrans
 
 public class BalleRigid : NetworkBehaviour // objet réseau
 {
-   public static BalleRigid instance; // Singleton
-   float maxDistanceX = 25f; // moitié de la largeur de la table, pour savoir si un but est compté
-   [SerializeField] private float nombreDeBonds; //compte du nombre de bonds de la balle // Servira plus tard
-   [SerializeField] private float maxSpeed; // si on veut limiter la vitesse max de la balle (inutilisé)
-// Création d'un singleton. Il ne doit y avoir qu'une seule balle.
-   private void Awake()
-   {
-       if (instance == null)
-       {
-           instance = this;
-           return;
-       }
-       Destroy(gameObject);
+    public static BalleRigid instance; // Singleton
+    float maxDistanceX = 25f; // moitié de la largeur de la table, pour savoir si un but est compté
+    [SerializeField] private float nombreDeBonds; //compte du nombre de bonds de la balle // Servira plus tard
+    [SerializeField] private float maxSpeed; // si on veut limiter la vitesse max de la balle (inutilisé)
+    public CamShaker camShaker;
+    // Création d'un singleton. Il ne doit y avoir qu'une seule balle.
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            return;
+        }
+        Destroy(gameObject);
 
-   }
+    }
 
 
-   /* Vérificaiton de la position de la balle pour voir si un but est compté
-   - Seul le serveur fait la validation
-   - Pas de validation si aucune partie en cours
+    /* Vérificaiton de la position de la balle pour voir si un but est compté
+    - Seul le serveur fait la validation
+    - Pas de validation si aucune partie en cours
 
-   - Détection de la position de la balle. S'il y a un but :
-   --- À implémenter On appelle la fonction pour augmenter le score du client hote (serveur) ou du client
-   --- On appelle la fonction LanceBalleMilieu qui va replacer la balle
-   */
-   void Update()
-   {
-       if (!IsServer) return;
+    - Détection de la position de la balle. S'il y a un but :
+    --- À implémenter On appelle la fonction pour augmenter le score du client hote (serveur) ou du client
+    --- On appelle la fonction LanceBalleMilieu qui va replacer la balle
+    */
+    void Update()
+    {
+        if (!IsServer) return;
 
-       if (!GameManager.instance.partieEnCours) return; // Il faudra créer cette variable dans le GameManager
+        if (!GameManager.instance.partieEnCours) return; // Il faudra créer cette variable dans le GameManager
 
-       //but client
-       if (transform.position.x < -maxDistanceX)
-       {
-           // Ici, il faudra aussi augmenter le score du joueur
-           ScoreManager.instance.AugmenteScoreClient();
-           LanceBalleMilieu();
-       }
+        //but client
+        if (transform.position.x < -maxDistanceX)
+        {
+            // Ici, il faudra aussi augmenter le score du joueur
+            ScoreManager.instance.AugmenteScoreClient();
+            LanceBalleMilieu();
+        }
 
-       //but serveur (hôte)
-       if (transform.position.x > maxDistanceX)
-       {
-           // Ici, il faudra aussi augmenter le score du joueur
-           ScoreManager.instance.AugmenteHoteScore();
-           LanceBalleMilieu();
-       }
-   }
+        //but serveur (hôte)
+        if (transform.position.x > maxDistanceX)
+        {
+            // Ici, il faudra aussi augmenter le score du joueur
+            ScoreManager.instance.AugmenteHoteScore();
+            LanceBalleMilieu();
+        }
+    }
 
-   /* Fonction qui amorce la séquence pour replacer la balle au milieu. Fonction publique appelée aussi par
-   le script GameManager. (À implémenter)
-   - Cacul du nombre de bonds à 0
-   - On désactive l'interpolation du NetworkTransform pour éviter de voir l'interpolation de position de la balle
-   - On replace la balle au centre de la table et on remet à 0 sa vélocité
-   - Lancement d'une coroutine qui replacera et relancera la balle seulement si la partie n'est pas terminée
-   */
-   public void LanceBalleMilieu()
-   {
+    /* Fonction qui amorce la séquence pour replacer la balle au milieu. Fonction publique appelée aussi par
+    le script GameManager. (À implémenter)
+    - Cacul du nombre de bonds à 0
+    - On désactive l'interpolation du NetworkTransform pour éviter de voir l'interpolation de position de la balle
+    - On replace la balle au centre de la table et on remet à 0 sa vélocité
+    - Lancement d'une coroutine qui replacera et relancera la balle seulement si la partie n'est pas terminée
+    */
+    public void LanceBalleMilieu()
+    {
         Debug.Log("LanceBalleMilieu() appelée");
-       nombreDeBonds = 0;
-       GetComponent<NetworkTransform>().Interpolate = false;
-       transform.position = new Vector3(0f, 0.5f, 0f);
-       GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, 0);
-       if (GameManager.instance.partieTerminee) return; // Il faudra créer cette variable dans le GameManager
-       StartCoroutine(NouvelleBalle());
-   }
+        nombreDeBonds = 0;
+        GetComponent<NetworkTransform>().Interpolate = false;
+        transform.position = new Vector3(0f, 0.5f, 0f);
+        GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, 0);
+        if (GameManager.instance.partieTerminee) return; // Il faudra créer cette variable dans le GameManager
+        StartCoroutine(NouvelleBalle());
+    }
 
-   /* Coroutine qui fait suite à la fonction LanceBalleMilieu()
-   - Pause d'une seconde
-   - On réactive l'interpolation du NetworkTransform pour des déplacements fluides côté client
-   - Vélocité de la balle tirée au hasard et application de la force sur la balle
+    /* Coroutine qui fait suite à la fonction LanceBalleMilieu()
+    - Pause d'une seconde
+    - On réactive l'interpolation du NetworkTransform pour des déplacements fluides côté client
+    - Vélocité de la balle tirée au hasard et application de la force sur la balle
+    */
+    IEnumerator NouvelleBalle()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        GetComponent<NetworkTransform>().Interpolate = true;
+
+        System.Random random = new System.Random();
+        float aleaX = random.Next(0, 2) == 0 ? -10 : 10; //  opérateur ternaire
+        float aleaZ = random.Next(0, 2) == 0 ? -10 : 10;
+
+        GetComponent<Rigidbody>().AddForce(aleaX, 0, aleaZ, ForceMode.Impulse);
+    }
+
+    /* Attention ne fonctionne pas sur le client. À utiliser seulement si détection OK seulement sur le serveur
+   Fonction qui calcule le nombre de bonds de la balle et qui augmente la vitesse de cette dernière à chaque
+   5 bonds.
+   On appele aussi la fonction Shake du script camShaker pour faire bouger la caméra lorsque la balle frappe un mur
+   ou une barre. Utilisation d'un RPC pour que tous les clients voient le shake de la caméra.
    */
-   IEnumerator NouvelleBalle()
-   {
-       yield return new WaitForSecondsRealtime(1f);
-       GetComponent<NetworkTransform>().Interpolate = true;
+    private void OnCollisionEnter(Collision infoCollisions)
+    {
+        if (infoCollisions.gameObject.tag == "Barre")
+        {
+            nombreDeBonds++;
 
-       System.Random random = new System.Random();
-       float aleaX = random.Next(0, 2) == 0 ? -10 : 10; //  opérateur ternaire
-       float aleaZ = random.Next(0, 2) == 0 ? -10 : 10;
+            if (nombreDeBonds > 5)
+            {
+                Vector3 velociteActuelle = GetComponent<Rigidbody>().linearVelocity;
+                GetComponent<Rigidbody>().AddForce(velociteActuelle * 5f, ForceMode.Force);
+                nombreDeBonds = 0;
+            }
+        }
 
-       GetComponent<Rigidbody>().AddForce(aleaX, 0, aleaZ, ForceMode.Impulse);
-   }
+        if (infoCollisions.gameObject.tag == "Barre" || infoCollisions.gameObject.tag == "Murs") Shake_Rpc();
+    }
+
+    /* Fonction RPC pour faire bouger la caméra
+    - Appelée par le serveur pour tous les clients */
+    [Rpc(SendTo.Everyone)]
+    private void Shake_Rpc()
+    {
+        if (camShaker == null) return;
+        camShaker.Shake(0.2f, 0.2f, 20f);
+    }
+
+
 }
